c892a484a793585a9a59717a651a40b2b9810c4b
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-07-03 11:10:26 -0300

    Commit da aula 6

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
   const nome = "Maria";
 
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
       <SayMyName nome="Thiago" />
       <SayMyName nome={nome} />
       <Pessoa 
